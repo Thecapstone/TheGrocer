83ea464fef5d6145f1fb63699ece0c24bf6a1d23
Thecapstone <thecapstonemogbolu@gmail.com>
2026-01-22 19:44:31 +0100

    installed files for vercel

--- TheGrocer/settings.py
+++ TheGrocer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9gr&nltz(@ak)-v)uidyg)p40_icmd+z@q%t1^)$bwnf*@n)nw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
